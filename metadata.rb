d5d302067a7365a2dbfa197108df6dda57a1f8b0
Jiri Barton <jbar@tele3.cz>
2015-06-07 15:13:24 +0200

    metadata

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name 'syntax-check'
+maintainer 'Pantheon Systems'
+maintainer_email 'engineering@getpantheon.com'
+license 'Apache 2.0'
+description 'Essential data checks'
+long_description 'Essential data checks'
+version '0.0.1'
+
